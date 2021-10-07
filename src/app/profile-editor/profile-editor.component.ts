daf170b14a9fe7a7f83d1db374f18c57e5e33c72
CriBeta <cristina.celiamagno@beta80group.it>
2021-10-07 12:48:11 +0200

    aggiunte spiegazioni form

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -8,33 +8,34 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class ProfileEditorComponent {
   profileForm = new FormGroup({
-    firstName: new FormControl(''),   //Use the constructor of FormControl to set its initial value, which in this case is an empty string. By creating these controls in your component class, you get immediate access to listen for, update, and validate the state of the form input.
-    lastName: new FormControl(''),
-    //To create a nested group in profileForm, add a nested address element to the form group instance.
-    address: new FormGroup({          // Changes in status and value from the nested form group propagate to the parent form group, maintaining consistency with the overall model.
+    firstName: new FormControl(''),   //Utilizzare il costruttore di FormControl per impostarne il valore iniziale, che in questo caso è una stringa vuota.
+    lastName: new FormControl(''),    //Creando questi controlli nella classe del componente, si ottiene l'accesso immediato per ascoltare, aggiornare e convalidare lo stato dell'input del form.
+    //Per creare un gruppo nidificato in profileForm, aggiungere un elemento indirizzo nidificato all'istanza del Form group.
+    address: new FormGroup({          // Le modifiche allo stato e al valore dal gruppo nidificato si propagano al gruppo padre, mantenendo la consistenza/coerenza con il modello generale.
       street: new FormControl(''),
       city: new FormControl(''),
       state: new FormControl(''),
       zip: new FormControl('')
     })
   });
-  /*The individual form controls are now collected within a group. A FormGroup instance provides its model value as an object reduced from the values of each control in the group. 
-  A form group instance has the same properties (such as value and untouched) and methods (such as setValue()) as a form control instance.*/
+  /*I singoli controlli del modulo sono ora raccolti all'interno di un gruppo. Un'istanza di FormGroup fornisce il valore del proprio modello come oggetto ridotto dai valori di ogni controllo nel gruppo.
+  Un'istanza del From group ha le stesse proprietà (come value e untouched) e metodi (come setValue()) di un'istanza del form control.*/
 
   onSubmit() {
-    //Da fare: Use EventEmitter to keep the form encapsulated and to provide the form value outside the component. 
+    //Utilizzare EventEmitter per mantenere il modulo incapsulato e per fornire il valore del modulo all'esterno del componente.
     console.log(this.profileForm.value);
   }
 
   /*
-    When updating the value for a form group instance that contains multiple controls, you might only want to update parts of the model. This section covers how to update specific parts of a form control data model.
-    There are two ways to update the model value:
-    Use the setValue() method to set a new value for an individual control. The setValue() method strictly adheres to the structure of the form group and replaces the entire value for the control.
-    Use the patchValue() method to replace any properties defined in the object that have changed in the form model.
+    Quando si aggiorna il valore per un'istanza del Form group che contiene più controlli, potrebbe essere necessario aggiornare solo parti del modello. 
+    Esistono due modi per aggiornare il valore del modello:
+    Utilizzare il metodo setValue() per impostare un nuovo valore per un singolo controllo.
+        Il metodo setValue() aderisce strettamente alla struttura del Form group e sostituisce l'intero valore per il controllo.
+    Utilizzare il metodo patchValue() per sostituire qualsiasi proprietà definita nell'oggetto che è stata modificata nel Form.
   */
 
     updateProfile() {
-      this.profileForm.patchValue({   //PatchValue() only updates properties that the form model defines.
+      this.profileForm.patchValue({   //PatchValue() aggiorna solo le proprietà definite dal Form model.
         firstName: 'Pippo',
         address: {
           street: '123 Drew Street'
